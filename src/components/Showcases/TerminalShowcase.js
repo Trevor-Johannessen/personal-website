af1dcf55154a3eee855a2a7946de4f3deacc54c0
Trevor Johannessen <91626575+Trevor-Johannessen@users.noreply.github.com>
2023-02-06 15:59:12 -0500

    made terminal work better with mobile

--- src/components/Showcases/TerminalShowcase.js
+++ src/components/Showcases/TerminalShowcase.js
@@ -4,33 +4,35 @@ import Terminal from "../Terminal";
 
 export default function TerminalShowcase(props){
 
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: {xs: 'column', md: 'row'},
+        minHeight: '60vh',
+        marginBottom:'20px',
+    }
 
     return(
         <Showcase>
             <h1 style={{marginBottom: '0px'}}>Terminal</h1>
             <h6 style={{marginTop:'0px'}}>Current Space Filler.</h6>
             <Box
-                style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    height: '60vh',
-                }}
+                sx={{...containerStyle}}
             >
                 <Box style={{display: 'grid', placeItems: 'center', alignContent: 'center'}}>
-                <Box
-                style={{
-                    backgroundColor: '#DEDEDE', 
-                    padding: "4px", 
-                    borderRadius:"10px",
-                    borderWidth: "3px",
-                    borderStyle: "solid",
-                    width: '40vw',
-                    margin: '5vw',
-                }}>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore quasi maxime quidem vero mollitia fugiat ducimus aliquid recusandae accusantium sed. Aperiam exercitationem, libero nam porro unde sint sunt quia eum facere maxime obcaecati, asperiores nesciunt, error ut magni fuga assumenda neque. Nam explicabo blanditiis quasi ab quas esse nesciunt vitae!
-                    </p>
-                </Box>
+                    <Box
+                    style={{
+                        backgroundColor: '#DEDEDE', 
+                        padding: "4px", 
+                        borderRadius:"10px",
+                        borderWidth: "3px",
+                        borderStyle: "solid",
+                        width: '40vw',
+                        margin: '5vw',
+                    }}>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore quasi maxime quidem vero mollitia fugiat ducimus aliquid recusandae accusantium sed. Aperiam exercitationem, libero nam porro unde sint sunt quia eum facere maxime obcaecati, asperiores nesciunt, error ut magni fuga assumenda neque. Nam explicabo blanditiis quasi ab quas esse nesciunt vitae!
+                        </p>
+                    </Box>
                 </Box>
                 <Box style={{width: '50vw', margin:'2.5vw', marginLeft: '0'}}>
                     <Terminal/>
